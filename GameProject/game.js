0c14f28cfe02162dfc6a5521b8f19c2df7e07dfa
zijunmei <zijun.mei1993@gmail.com>
2020-02-19 13:55:44 -0600

    Create game.js

---
+++ GameProject/game.js
@@ -0,0 +1,28 @@
+/* import statements */
+
+/* Setup Scenes, game Objects, components, and behaviors */
+
+let currentScene = startScene;
+
+let canv, ctx;
+
+function main() {
+    canv = document.querySelector("#canv");
+    ctx = canv.getContext('2d');
+
+    /* setup game loop speed */
+}
+
+function gameLoop() {
+    /* setup roles of game loop */
+}
+
+function update() {
+    /* What does the update function do? */
+}
+
+function draw(ctx) {
+    /* What does the draw function do? */
+}
+
+main();
